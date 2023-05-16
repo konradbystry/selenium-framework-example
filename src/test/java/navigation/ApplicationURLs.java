d22a9007e056679f5b196c879df12c761d6882da
unknown <k.bystrzykowski@testspring.pl>
2023-05-16 12:22:01 +0200

    Added url for example table test page

--- src/test/java/navigation/ApplicationURLs.java
+++ src/test/java/navigation/ApplicationURLs.java
@@ -5,4 +5,6 @@ import configuration.AppProperties;
 public class ApplicationURLs {
     public static final String APPLICATION_URL = AppProperties.getAllUrl();
     public static final String LOGIN_URL = APPLICATION_URL + "actions/Account.action?signonForm=";
+
+    public static final String TABLE_EXAMPLE = "http://theinternet.przyklady.javastart.pl/tables";
 }
