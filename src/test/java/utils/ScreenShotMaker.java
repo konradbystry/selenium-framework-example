5bfc31b5a0455630cdd5432bbc5e30276ec84fc5
unknown <k.bystrzykowski@testspring.pl>
2023-05-16 10:52:55 +0200

    Created method for saving screenshots as files

--- src/test/java/utils/ScreenShotMaker.java
+++ src/test/java/utils/ScreenShotMaker.java
@@ -2,8 +2,12 @@ package utils;
 
 import driver.manager.DriverManager;
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ScreenShotMaker {
 
@@ -11,6 +15,16 @@ public class ScreenShotMaker {
     public static byte[] makeScreenShot() {
         byte[] screenshotFile = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
         return screenshotFile;
+    }
+
+    public static void makeScreenShotAndSaveAsFile() {
+        File scrFile = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(scrFile, new File("Screenshots/selenium_screenshot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
